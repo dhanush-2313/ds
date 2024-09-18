07d1e2df858f749a91576bc7fa415c397479a256
dhanush-2313 <dhanusu006@gmail.com>
2024-09-19 00:38:37 +0530

    palindrome

--- 8-rec.cpp
+++ 8-rec.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void swapFunc(int i, int arr[], int n)
+// void swapFunc(int i, int arr[], int n)
+// {
+//     if (i >= n / 2)
+//     {
+//         return;
+//     }
+//     swap(arr[i], arr[n - i - 1]);
+//     swapFunc(i + 1, arr, n);
+// }
+
+bool palindrome(int i, string &a)
 {
-    if (i >= n / 2)
+    if (i >= a.size() / 2)
     {
-        return;
+        return true;
     }
-    swap(arr[i], arr[n - i - 1]);
-    swapFunc(i + 1, arr, n);
+    if (a[i] != a[a.size() - i - 1])
+    {
+        return false;
+    }
+    return palindrome(i + 1, a);
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    swapFunc(0, arr, n);
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    string a = "madam";
+    cout << palindrome(0, a);
+    return 0;
 }
