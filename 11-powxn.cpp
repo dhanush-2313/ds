fc433a099d4efa4c4eae432b40537896dc5e7f5b
dhanush-2313 <dhanusu006@gmail.com>
2024-09-24 22:50:37 +0530

    power

---
+++ 11-powxn.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+using namespace std;
+
+double power(int x, int n)
+{
+    long binForm = n;
+    double ans = 1;
+    if (n < 0)
+    {
+        x = 1 / x;
+        binForm = -binForm;
+    }
+    while (binForm > 0)
+    {
+        if (binForm % 2 == 1)
+        {
+            ans *= x;
+        }
+        x *= x;
+        binForm /= 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    power(2, 10);
+}
