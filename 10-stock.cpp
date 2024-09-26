1ba0cd2bb29238b8b121405c185416855001f8fe
dhanush-2313 <dhanusu006@gmail.com>
2024-09-26 16:44:07 +0530

    buy and sell stock

--- 10-stock.cpp
+++ 10-stock.cpp
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
 using namespace std;
+
+int stock(vector<int> a)
+{
+    int bestBuy = a[0];
+    int maxProfit = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] > bestBuy)
+        {
+            maxProfit = max(maxProfit, a[i]);
+        }
+
+        bestBuy = min(bestBuy, a[i]);
+    }
+    return maxProfit;
+};
 
 int main()
 {
+    vector<int> a = {7, 1, 4, 6, 5};
+    cout << stock(a) << endl;
 }
