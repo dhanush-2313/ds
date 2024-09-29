92c482922fb73cfdba2afef53bb580e60556ab44
dhanush-2313 <dhanusu006@gmail.com>
2024-09-29 18:51:55 +0530

    rotate array by d places

--- 21-rotateArray.cpp
+++ 21-rotateArray.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-void rotateArray(vector<int> &arr){
-int temp = arr[0];
-for(int i=1;i<arr.size();i++){
-arr[i-1]=arr[i];
+// brute force approach
+void Rotatetoleft(int arr[], int n, int k)
+{
+    if (n == 0)
+        return;
+    k = k % n;
+    if (k > n)
+        return;
+    int temp[k];
+    for (int i = 0; i < k; i++)
+    {
+        temp[i] = arr[i];
+    }
+    for (int i = 0; i < n - k; i++)
+    {
+        arr[i] = arr[i + k];
+    }
+    for (int i = n - k; i < n; i++)
+    {
+        arr[i] = temp[i - n + k];
+    }
 }
-arr[arr.size()-1]=temp;
 
+// most optimal approach
+void optimalrotate(int arr[], int k, int n)
+{
+    reverse(arr, arr + k);
+    reverse(arr + k, arr + n);
+    reverse(arr, arr + n);
 }
 
+int main()
+{
+    int n = 7;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int k = 2;
+    // Rotatetoleft(arr, n, k);
+    // cout << "After Rotating the elements to left " << endl;
 
-
-
-int main(){
-
-vector<int> arr={1,2,3,4,5};
-rotateArray(arr);
-for(auto it:arr)
-cout << it << " " ;
-
-
-return 0;
+    optimalrotate(arr, 3, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
 
+// reverse function for reference
+/*
+void reverseArray(int arr[],int start,int end){
+while(start<=end){
+int temp = arr[start];
+arr[end]= arr[start];
+start++;
+end--;
+}
+}
+*/
