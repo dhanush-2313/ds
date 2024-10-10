3b9206525253ba089cd716689716d157c24db2bd
dhanush-2313 <dhanusu006@gmail.com>
2024-10-10 10:26:09 +0530

    30-better

--- 30-sortarray0s1s2s.cpp
+++ 30-sortarray0s1s2s.cpp
@@ -5,14 +5,13 @@ using namespace std;
 void sortt(vector<int> &arr, int n)
 {
     int cnt0 = 0, cnt1 = 0;
-    for (int i = 0; i > n; i++)
+    for (int i = 0; i < n; i++)
     {
         if (arr[i] == 0)
             cnt0++;
         if (arr[i] == 1)
             cnt1++;
     }
-
     for (int i = 0; i < cnt0; i++)
     {
         arr[i] = 0;
@@ -29,7 +28,7 @@ void sortt(vector<int> &arr, int n)
 
 int main()
 {
-    vector<int> arr = {0, 1, 2, 9, 1, 2, 1, 2, 0, 0, 0, 1};
+    vector<int> arr = {0, 1, 2, 0, 1, 2, 1, 2, 0, 0, 0, 1};
     sortt(arr, arr.size());
     for (auto it : arr)
     {
