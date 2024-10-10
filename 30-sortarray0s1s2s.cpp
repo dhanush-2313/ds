893d5a45afb593a72946aa8c610f5df6bc7892e2
dhanush-2313 <dhanusu006@gmail.com>
2024-10-10 15:37:20 +0530

    30-optimal

--- 30-sortarray0s1s2s.cpp
+++ 30-sortarray0s1s2s.cpp
@@ -1,37 +1,42 @@
-#include <iostream>
-#include <vector>
+
+#include <bits/stdc++.h>
 using namespace std;
 
-void sortt(vector<int> &arr, int n)
+void sortArray(vector<int> &arr, int n)
 {
-    int cnt0 = 0, cnt1 = 0;
-    for (int i = 0; i < n; i++)
+
+    int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high)
     {
-        if (arr[i] == 0)
-            cnt0++;
-        if (arr[i] == 1)
-            cnt1++;
-    }
-    for (int i = 0; i < cnt0; i++)
-    {
-        arr[i] = 0;
-    }
-    for (int i = cnt0; i < cnt1 + cnt0; i++)
-    {
-        arr[i] = 1;
-    }
-    for (int i = cnt0 + cnt1; i < n; i++)
-    {
-        arr[i] = 2;
+        if (arr[mid] == 0)
+        {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
     }
 }
 
 int main()
 {
-    vector<int> arr = {0, 1, 2, 0, 1, 2, 1, 2, 0, 0, 0, 1};
-    sortt(arr, arr.size());
-    for (auto it : arr)
+    int n = 6;
+    vector<int> arr = {0, 2, 1, 2, 0, 1};
+    sortArray(arr, n);
+    cout << "After sorting:" << endl;
+    for (int i = 0; i < n; i++)
     {
-        cout << it << " ";
+        cout << arr[i] << " ";
     }
+    cout << endl;
+    return 0;
 }
